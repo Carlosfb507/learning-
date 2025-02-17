55d01ae59776c6d7e51974b0f9bd5fb1055facb0
carlos <carlosfb508>
2025-02-17 11:24:34 -0500

    este archivo contiene operaciones basicas

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+print("CALCULADORA BASICA")
+
+num1=5
+num2=4
+suma = num1 + num2
+resta = num1 - num2
+multi= num1*num2
+div= num1/num2
+
+print ("la suma es", suma)
+print ("la resta es",resta)
+print ("la mult es", multi)
+print ("la div es",div)
+
+
+
+
+
